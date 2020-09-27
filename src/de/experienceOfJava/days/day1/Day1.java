e09a6852a335dffadf61dd50555054d7e2623c99
DJE <dennis.jeschke@gmail.com>
2020-09-27 18:42:51 +0200

    implement day1 part 1 - non-static import

--- src/de/experienceOfJava/days/day1/Day1.java
+++ src/de/experienceOfJava/days/day1/Day1.java
@@ -11,19 +11,21 @@ import de.experienceOfJava.service.inout.ArrayListReaderService;
 
 public class Day1 implements Day {
     public static void calculate() throws FileNotFoundException {
-    // Moduleinput. Alle Werte aus day1.txt in ein Array laden.
+        // Moduleinput. Alle Werte aus day1.txt in ein Array laden.
         ArrayListReaderService readerService = new ArrayListReaderService("src/de/experienceOfJava/resource/day1.txt");
         ArrayList<String> result = readerService.getArrayList();
 
         // Module zu benötigten Treibstoff umrechnen. Schleife - Addiere jedes Modul zum benötigten Gesantkraftstoff
         Day1FuelMassCalcService fuelService = new Day1FuelMassCalcService();
-   //     Long fuelMassModule = Day1FuelMassCalcService.moduleFuelMass(result);
-        }
+        Long fuelMassModule = fuelService.moduleFuelMass(result);
 
-    // Output Gesamtkraftstoff
-       // System.out.println("benötigte Gesamtkraftstoffmenge für alle Module: " + fuelMassModule + " (Part One)");
+        // Output Gesamtkraftstoff
+        // part 1
+        System.out.println("benötigte Gesamtkraftstoffmenge für alle Module: " + fuelMassModule + " (Part One)");
+        // part 2
         //System.out.println("benötigte Gesamtkraftstoffmenge für alle Module: " + fuelMass + " (Part Two)");
-
     }
+
+}
 
 
